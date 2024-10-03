70c73fa94f036a86d0547119601a702b0b3b3c27
ju78832 <junedgreat89@gmail.com>
2024-10-03 13:30:33 +0530

    Update the changes

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -106,7 +106,7 @@ const Chat = () => {
           </Typography>
           <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
             You can ask some questions related to Knowledge, Business, Advices,
-            Education, etc. But avoid sharing personal information
+            Education, etc. But avoid to share your personal details.
           </Typography>
           <Button
             onClick={handleDeleteChats}
